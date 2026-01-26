c5aab852fcd2ff21056611dc39a0fe933a39aca9
Herasi Silva <herasidesweb@gmail.com>
2026-01-26 06:18:13 -0400

    Update test_category.php

--- test_category.php
+++ test_category.php
@@ -1,27 +1,34 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ob_start();
 
 echo "1. Inicio<br>";
 
-echo "2. Cargando config...<br>";
 require_once __DIR__ . '/config/config.php';
-echo "3. Config OK<br>";
+echo "2. Config OK<br>";
 
-echo "4. Cargando model...<br>";
 require_once __DIR__ . '/models/category.php';
-echo "5. Model OK<br>";
+echo "3. Model OK<br>";
 
-echo "6. Cargando Security...<br>";
 require_once __DIR__ . '/utils/Security.php';
-echo "7. Security OK<br>";
+echo "4. Security OK<br>";
 
-echo "8. Cargando BunnyStorage...<br>";
 require_once __DIR__ . '/utils/BunnyStorage.php';
-echo "9. BunnyStorage OK<br>";
+echo "5. BunnyStorage OK<br>";
 
-echo "10. Cargando ImageCompressor...<br>";
 require_once __DIR__ . '/utils/ImageCompressor.php';
-echo "11. ImageCompressor OK<br>";
+echo "6. ImageCompressor OK<br>";
 
-echo "<br>TODOS LOS ARCHIVOS CARGARON CORRECTAMENTE";
+echo "7. Creando instancias...<br>";
+
+$categoryModel = new Category();
+echo "8. Category instance OK<br>";
+
+$bunnyStorage = new BunnyStorage();
+echo "9. BunnyStorage instance OK<br>";
+
+$imageCompressor = new ImageCompressor(800, 800, 85);
+echo "10. ImageCompressor instance OK<br>";
+
+echo "<br>TODO FUNCIONA CORRECTAMENTE";
