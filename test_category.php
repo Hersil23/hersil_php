c28fd5fab4900389238d9cc9f29fd083e3bd11aa
Herasi Silva <herasidesweb@gmail.com>
2026-01-26 06:19:24 -0400

    Update test_category.php

--- test_category.php
+++ test_category.php
@@ -5,30 +5,8 @@ ob_start();
 
 echo "1. Inicio<br>";
 
-require_once __DIR__ . '/config/config.php';
-echo "2. Config OK<br>";
+echo "2. Cargando CategoryController...<br>";
+require_once __DIR__ . '/controllers/CategoryController.php';
+echo "3. CategoryController cargado OK<br>";
 
-require_once __DIR__ . '/models/category.php';
-echo "3. Model OK<br>";
-
-require_once __DIR__ . '/utils/Security.php';
-echo "4. Security OK<br>";
-
-require_once __DIR__ . '/utils/BunnyStorage.php';
-echo "5. BunnyStorage OK<br>";
-
-require_once __DIR__ . '/utils/ImageCompressor.php';
-echo "6. ImageCompressor OK<br>";
-
-echo "7. Creando instancias...<br>";
-
-$categoryModel = new Category();
-echo "8. Category instance OK<br>";
-
-$bunnyStorage = new BunnyStorage();
-echo "9. BunnyStorage instance OK<br>";
-
-$imageCompressor = new ImageCompressor(800, 800, 85);
-echo "10. ImageCompressor instance OK<br>";
-
-echo "<br>TODO FUNCIONA CORRECTAMENTE";
+echo "<br>TODO FUNCIONA";
