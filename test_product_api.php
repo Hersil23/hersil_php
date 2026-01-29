2560b7b3b9e35f4a754e28670d967585a8bfb13d
Herasi Silva <herasidesweb@gmail.com>
2026-01-28 22:03:04 -0400

    Update test_product_api.php

--- test_product_api.php
+++ test_product_api.php
@@ -2,13 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__ . '/config/config.php';
-require_once __DIR__ . '/models/product.php';
+require_once __DIR__ . '/controllers/ProductController.php';
 
-echo "Config OK<br>";
-
-$productModel = new Product();
-echo "Model OK<br>";
-
-$products = $productModel->getAll();
-echo "Products: " . count($products);
+echo "Controller OK";
